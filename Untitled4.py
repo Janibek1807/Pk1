34756dba07b900883bf04c713dc6cc70819c6a13
Janibek1807 <78403020+Janibek1807@users.noreply.github.com>
2021-03-16 13:25:21 +0600

    Add files via upload

---
+++ Untitled4.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[5]:
+
+
+n=(int(input('n=')))
+for i in range(n):
+    for j in range(i):
+        print('#',end="")
+    print('')
+for i in range(n,0,-1):
+    for j in range(i):
+        print('#',end="")
+    print('')
+
