64b4ee696ad1dbabd894e7fc3e0182633988be69
Janibek1807 <78403020+Janibek1807@users.noreply.github.com>
2021-03-16 12:48:19 +0600

    Add files via upload

---
+++ Untitled2.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+a=5
+b=5.5
+c="9.1"
+res=float(a)+b+float(c)
+print(res)
+
+
+# In[2]:
+
+
+import math
+x=input ("Потери=")
+x=int(x)
+y=input ("Прибыль=")
+y=int(y)
+if x>y:
+    res=x-y
+    print ("Ваши убытки составили:",res)
+elif x<y:
+    res=y-x
+    print ("Ваши прибыль составили:",res) 
+
+
+# In[ ]:
+
+
+
+
